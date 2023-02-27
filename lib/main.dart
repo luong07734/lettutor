60e07544bdf272ac66dfa1cf7c3e815054d12929
luong07734 <le.ky.luong@gmail.com>
2023-02-27 22:01:05 +0700

    Drawer UI
    - Add drawer UI using flutter_advanced_drawer library

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
 import 'package:lettutor/constants/color_manager.dart';
 import 'package:lettutor/view/authentication/pages/forgot_password.dart';
 import 'package:lettutor/view/authentication/pages/log_in.dart';
@@ -27,71 +28,178 @@ class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   int _page = 0;
   GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+  final _advancedDrawerController = AdvancedDrawerController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: const IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
+    return AdvancedDrawer(
+      backdropColor: Colors.blueGrey,
+      controller: _advancedDrawerController,
+      animationCurve: Curves.easeInOut,
+      animationDuration: const Duration(milliseconds: 300),
+      animateChildDecoration: true,
+      rtlOpening: false,
+      // openScale: 1.0,
+      disabledGestures: false,
+      childDecoration: const BoxDecoration(
+        // NOTICE: Uncomment if you want to add shadow behind the page.
+        // Keep in mind that it may cause animation jerks.
+        // boxShadow: <BoxShadow>[
+        //   BoxShadow(
+        //     color: Colors.black12,
+        //     blurRadius: 0.0,
+        //   ),
+        // ],
+        borderRadius: const BorderRadius.all(Radius.circular(16)),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: _handleMenuButtonPressed,
+            icon: ValueListenableBuilder<AdvancedDrawerValue>(
+              valueListenable: _advancedDrawerController,
+              builder: (_, value, __) {
+                return AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 250),
+                  child: Icon(
+                    value.visible ? Icons.clear : Icons.menu,
+                    key: ValueKey<bool>(value.visible),
+                  ),
+                );
+              },
+            ),
+          ),
+          title: const Text('Example title'),
+          actions: const [
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null,
+            ),
+          ],
         ),
-        title: const Text('Example title'),
-        actions: const [
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
+        // body is the majority of the screen.
+        body: Container(
+          // color: Colors.blueAccent,
+          child:
+              HomePage(page: _page, bottomNavigationKey: _bottomNavigationKey),
+        ),
+        floatingActionButton: const FloatingActionButton(
+          tooltip: 'Add', // used by assistive technologies
+          onPressed: null,
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Colors.white,
+          color: Colors.blueAccent,
+          items: const <Widget>[
+            Icon(
+              Icons.home,
+              size: 30,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.message,
+              size: 30,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.lock_clock,
+              size: 30,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.people,
+              size: 30,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.settings,
+              size: 30,
+              color: Colors.white,
+            ),
+          ],
+          onTap: (index) {
+            //Handle button tap
+            setState(() {
+              _page = index;
+            });
+          },
+        ),
       ),
-      // body is the majority of the screen.
-      body: Container(
-        // color: Colors.blueAccent,
-        child: HomePage(page: _page, bottomNavigationKey: _bottomNavigationKey),
-      ),
-      floatingActionButton: const FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        onPressed: null,
-        child: Icon(Icons.add),
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.white,
-        color: Colors.blueAccent,
-        items: const <Widget>[
-          Icon(
-            Icons.home,
-            size: 30,
-            color: Colors.white,
+      drawer: SafeArea(
+        child: Container(
+          child: ListTileTheme(
+            textColor: Colors.white,
+            iconColor: Colors.white,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                  width: 128.0,
+                  height: 128.0,
+                  margin: const EdgeInsets.only(
+                    top: 24.0,
+                    bottom: 64.0,
+                  ),
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    color: Colors.black26,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Image.asset(
+                    'assets/images/flutter_logo.png',
+                  ),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: Icon(Icons.schedule),
+                  title: Text('Schedule History'),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: Icon(Icons.favorite),
+                  title: Text('Favourite Teachers'),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: Icon(Icons.co_present),
+                  title: Text('Become a teacher'),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: Icon(Icons.settings),
+                  title: Text('Settings'),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: Icon(Icons.logout),
+                  title: Text('Logout'),
+                ),
+                Spacer(),
+                DefaultTextStyle(
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white54,
+                  ),
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 16.0,
+                    ),
+                    child: Text('Let tutor | Online tutoring system'),
+                  ),
+                ),
+              ],
+            ),
           ),
-          Icon(
-            Icons.message,
-            size: 30,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.lock_clock,
-            size: 30,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.people,
-            size: 30,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.settings,
-            size: 30,
-            color: Colors.white,
-          ),
-        ],
-        onTap: (index) {
-          //Handle button tap
-          setState(() {
-            _page = index;
-          });
-        },
+        ),
       ),
     );
+  }
+
+  void _handleMenuButtonPressed() {
+    // NOTICE: Manage Advanced Drawer state through the Controller.
+    // _advancedDrawerController.value = AdvancedDrawerValue.visible();
+    _advancedDrawerController.showDrawer();
   }
 }
